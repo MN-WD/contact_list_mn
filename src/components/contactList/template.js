16e90738cd04603f02b1a8f20d4022e4bdccdc3c
MN <bmdaetl@gmail.com>
2025-09-28 20:33:56 +0200

    Mise en place des components

---
+++ src/components/contactList/template.js
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export default function getTemplate (contactList) {
+  return `
+      <h1>ContactList</h1>
+      <ul class="contact-list"></ul>
+      <p>Template contact list ici.</p>
+    `;
+}
